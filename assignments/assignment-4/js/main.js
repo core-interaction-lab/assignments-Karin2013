ca415f1dcfa12b55d711bd54efae13d25fff9e4d
Yi Song <songy703@newschool.edu>
2022-04-22 10:05:35 -0400

    update

---
+++ assignments/assignment-4/js/main.js
@@ -0,0 +1,15 @@
+const albumContainer = document.getElementById('album-container')
+const fetchContent = async (fetchUrl) => {
+    const response = await fetch(fetchUrl).then(data => data.json());
+    console.log(response.items);
+
+
+    response.items.forEach(item => {
+        console.log(item);
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', item.images[0].url);
+        albumContainer.append(imgEl);
+    });
+};
+
+fetchContent('https://interactionlab.space/data/assignment-4-1.json');
