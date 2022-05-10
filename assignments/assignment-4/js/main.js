272c24c163729c198dc5ed3d94b7c9cd0f14d119
Yi Song <songy703@newschool.edu>
2022-05-10 16:59:04 -0400

    update

--- assignments/assignment-4/js/main.js
+++ assignments/assignment-4/js/main.js
@@ -1,15 +1,52 @@
-const albumContainer = document.getElementById('album-container')
+const albumContainer = document.getElementById('album-container');
+
 const fetchContent = async (fetchUrl) => {
-    const response = await fetch(fetchUrl).then(data => data.json());
-    console.log(response.items);
+    return fetch(fetchUrl).then(data => data.json ());
+
+};
 
 
-    response.items.forEach(item => {
+const buildAlbums = albums => {
+    albums.forEach(item => {
         console.log(item);
         const imgEl = document.createElement('img');
         imgEl.setAttribute('src', item.images[0].url);
-        albumContainer.append(imgEl);
+        imgEl.classList.add("photo")
+
+        const container = document.createElement('article');
+        
+
+        const nameEL = document.createElement('p');
+        nameEL.innerHTML = item.name;
+        nameEL.classList.add("name");
+
+        const release_dateEl = document.createElement('p');
+        release_dateEl.innerHTML = item.release_date;
+        release_dateEl.classList.add("date");
+
+        const total_tracksEl = document.createElement('p');
+        total_tracksEl.innerHTML = item.total_tracks;
+        total_tracksEl.classList.add("total_tracks");
+
+        const urlEl = document.createElement('a');
+        urlEl.innerHTML = item.href;
+        urlEl.classList.add("url");
+        
+        container.append(imgEl);
+        container.append(nameEL);
+        container.append(release_dateEl);
+        container.append(total_tracksEl);
+        container.append(urlEl);
+        albumContainer.append(container);
     });
+}
+
+const url1 = 'https://interactionlab.space/data/assignment-4-1.json';
+
+
+const main = async () => {
+    const response1 = await fetchContent(url1);
+    buildAlbums(response1.items);
 };
 
-fetchContent('https://interactionlab.space/data/assignment-4-1.json');
+main();
